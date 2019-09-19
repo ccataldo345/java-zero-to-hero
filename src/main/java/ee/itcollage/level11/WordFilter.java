84403fedcb819b8b1bff904bccde0e81ca482fb3
Christian Cataldo <epost345@gmail.com>
2019-09-19 17:55:51 +0300

    level 11 done

--- src/main/java/ee/itcollage/level11/WordFilter.java
+++ src/main/java/ee/itcollage/level11/WordFilter.java
@@ -8,30 +8,83 @@ public class WordFilter {
     //todo fix tests
 
     public List<Word> getNouns(List<Word> words) {
-        return words;
+        List<Word> nouns = new ArrayList<>();
+        for (Word word : words) {
+            //if (word.getWordType().equals(Word.WordType.NOUN)) {
+            if (word.getWordType().isNoun()) {
+                nouns.add(word);
+            }
+        }
+        return nouns;
     }
 
     public Word getFirstVerb(List<Word> words) {
-        return words.get(0);
+        List<Word> firstVerb = new ArrayList<>();
+        for (Word word : words) {
+            // if (word.getWordType().equals(Word.WordType.VERB)) {
+            if (word.getWordType().isVerb()) {
+                firstVerb.add(word);
+                return firstVerb.get(0);
+            }
+        }
+        return null;
     }
 
     public Word getFirstOfType(List<Word> words, Word.WordType type) {
-        return words.get(0);
+        List<Word> firstType = new ArrayList<>();
+        for (Word word : words) {
+            // if (word.getWordType().equals(Word.WordType.VERB)) {
+            if (word.getWordType().equals(type)) {
+                firstType.add(word);
+                return firstType.get(0);
+            }
+        }
+        return null;
     }
 
     public List<Word> getAllNotNouns(List<Word> words) {
-        return words;
+        List<Word> notNouns = new ArrayList<>();
+        for (Word word : words) {
+            //if (word.getWordType().equals(Word.WordType.NOUN)) {
+            if (!word.getWordType().isNoun()) {
+                notNouns.add(word);
+            }
+        }
+        return notNouns;
     }
 
-    public List<String> getNounStrings(List<Word> words){
-        return new ArrayList<>();
+    public List<String> getNounStrings(List<Word> words) {
+        List<String> nounsString = new ArrayList<>();
+        for (Word word : words) {
+            //if (word.getWordType().equals(Word.WordType.NOUN)) {
+            if (word.getWordType().isNoun()) {
+                nounsString.add(word.getWord());
+            }
+        }
+        return nounsString;
     }
 
     public String getFirstVerbString(List<Word> words) {
-        return "";
+        List<String> firstVerbString = new ArrayList<>();
+        for (Word word : words) {
+            // if (word.getWordType().equals(Word.WordType.VERB)) {
+            if (word.getWordType().isVerb()) {
+                firstVerbString.add(word.getWord());
+                return firstVerbString.get(0);
+            }
+        }
+        return null;
     }
 
     public String getFirstStringOfType(List<Word> words, Word.WordType type) {
-        return "";
+        List<String> firstStringType = new ArrayList<>();
+        for (Word word : words) {
+            // if (word.getWordType().equals(Word.WordType.VERB)) {
+            if (word.getWordType().equals(type)) {
+                firstStringType.add(word.getWord());
+                return firstStringType.get(0);
+            }
+        }
+        return null;
     }
 }
