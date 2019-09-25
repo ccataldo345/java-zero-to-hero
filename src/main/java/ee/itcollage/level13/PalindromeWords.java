85177f9cc8501ea527fbabaa138022181f760215
Christian Cataldo <epost345@gmail.com>
2019-09-25 15:30:58 +0300

    level 13 done

--- src/main/java/ee/itcollage/level13/PalindromeWords.java
+++ src/main/java/ee/itcollage/level13/PalindromeWords.java
@@ -1,6 +1,9 @@
 package ee.itcollage.level13;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PalindromeWords {
 
@@ -14,14 +17,33 @@ public class PalindromeWords {
      * cow (reversed woc) is not a palindrome
      */
     public AnalysisResult analyzePalindromes(List<String> strings){
-        return new AnalysisResult();
+        List<String> palindrome = new ArrayList<>();
+        List<String> otherWords = new ArrayList<>();
+        for (String string : strings) {
+            if(string.equals(reversedString(string))) {
+                palindrome.add(string);
+            } else {
+                otherWords.add(string);
+            }
+        }
+
+        //v2
+        Map<Boolean, List<String>> collect = strings.stream()
+                .collect(Collectors.partitioningBy(string -> string.equals(reversedString(string))));
+        new AnalysisResult(collect.get(true), collect.get(false));
+
+        return new AnalysisResult(palindrome, otherWords);
+    }
+
+    private static String reversedString(String string) {
+        return new StringBuilder(string).reverse().toString();
     }
 
     public static void main(String[] args) {
         String palidrome = "radar";
-        String reversedPalidrome = new StringBuilder(palidrome).reverse().toString();
+        String reversedPalidrome = reversedString(palidrome);
         String other = "other";
-        String otherReversed = new StringBuilder(other).reverse().toString();
+        String otherReversed = reversedString(other);
 
         System.out.println(String.format("%s %s", palidrome, reversedPalidrome));
         System.out.println(String.format("%s %s", other, otherReversed));
